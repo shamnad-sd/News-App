3d452fd8809eaf63a44c865382515561cca6fdcb
SdCode000 <shamnad2nk@gmail.com>
2024-12-06 00:06:40 +0530

    NewsApp

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,16 +1,41 @@
-import React from 'react'
-import techImg from '../assets/images/tech.jpg'
-import worldImg from '../assets/images/world.jpg'
-import sportsImg from '../assets/images/sports.jpg'
-import scienceImg from '../assets/images/science.jpg'
-import HealthImg from '../assets/images/health.jpg'
-import entertainmentImg from '../assets/images/entertainment.jpg'
-import nationImg from '../assets/images/nation.jpg'
+import React, { useEffect, useState } from 'react'
+import noImg from '../assets/images/no-img.png'
 import './News.css'
+import axios from 'axios'
+
 
 
 
 const News = () => {
+
+    const [headline , setHeadline]=useState(null)
+
+    const [news,setNews]=useState([])
+
+    useEffect(()=>{
+        const fetchNews = async () => {
+            const url ="https://gnews.io/api/v4/top-headlines?category=general&lang=en&apikey=ff18467843face6da195189500c10392";
+            const response = await axios.get(url)
+            
+
+            const fetchedNews = response.data.articles;
+            
+            fetchedNews.forEach((article)=>{
+                if (!article.image) {
+                    article.image = noImg;
+                }
+            })
+
+            setHeadline(fetchedNews[0])
+            setNews(fetchedNews.slice(1 , 7))
+        }
+
+        
+
+        fetchNews()
+    },[])
+
+
     return (
         <div className='news-app'>
             <div className="news-header">
@@ -34,56 +59,32 @@ const News = () => {
                     </div>
                 </nav>
                 <div className="news-section">
-                    <div className="headline">
-                        <img src={techImg} alt="Headline Image" />
+                    {headline && (
+                        <div className="headline">
+                        <img src={headline.image || noImg} alt={headline.title} />
                         <h2 className="headline-title">
-                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente, rerum.</h2>
+                        {headline.title}</h2>
                     </div>
+                    )}
+                    
                     <div className="news-grid">
+                    {news.map((article, index)=>(
 
-                        <div className="news-grid-item">
-                            <img src={worldImg} alt="News Image" />
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+                        <div className="news-grid-item" key={index}>
+                            <img src={article.image || noImg} alt={article.title} />
+                            <h3>{article.title}</h3>
                         </div>
-
-                        <div className="news-grid-item">
-                            <img src={sportsImg} alt="News Image" />
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                        </div>
-
-                        <div className="news-grid-item">
-                            <img src={scienceImg} alt="News Image" />
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                        </div>
-
-                        <div className="news-grid-item">
-                            <img src={HealthImg} alt="News Image" />
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                        </div>
-
-                        <div className="news-grid-item">
-                            <img src={entertainmentImg} alt="News Image" />
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                        </div>
-
-                        <div className="news-grid-item">
-                            <img src={nationImg} alt="News Image" />
-                            <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
-                        </div>
-
-
-
-                        
+                    ))}   
                     </div>
                 </div>
             </div>
             <footer>
                 <p className="copyright">
                     <span>News App</span>
-                    <p>&copy; All Rights Reserved.
+                </p>
+                <p>&copy; All Rights Reserved.
                         By Code And Create
                     </p>
-                </p>
             </footer>
         </div>
     )
